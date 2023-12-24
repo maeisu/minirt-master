845f1983091e7d8d1f49e788ee60de2422ab4542
Suzuka Maei <smaei@c1r20s4.42tokyo.jp>
2023-12-24 17:57:16 +0900

    test

--- test.c
+++ test.c
@@ -68,62 +68,49 @@ int main()
 
   //↓球体の設定
   // 視点座標
-  double Pv[3] = {0.0, 0.0, -5.0};
+  double Pe[3] = {0, 0, -500.0};
   // 中心座標
-  double Pc[3] = {0.0, 0.0, 5.0};
-  double R = 1.0;
-
-
-  double Pvc[3];
-  int k;
+  double Pc[3] = {0, 0, 0.0};
+  // 半径
+  double R = 70.0;
 
   double A, B2, C, D;
 
-  double PL[3] = {20000,20000, -20000}; // 光源座標
-  double P[3], N[3], vL[3], vR[3];
-  double cosA;
-  double t0;
-  double t;
-
-  double w, cosG;
-
-  double Pw[3]; //視線方向ベクトル
-  int i, j;
-
-  double Kd = 1.0, Ks = 0.7, le = 0.1, l = 255.0;
-  double RGB[2][3] = {{0, 0,205}, {0, 255, 0}};
+  //視線方向ベクトル
+  double Pw[3];
+  int y, x;
   int RGB_back[3] = {176, 224,230};
   int color[3] = {255,0,0};
+  int test_color[3] = { 0,0,255 };
   double De[3];
   double Vtmp[3];
 
-  for (i = 0; i <= (HEIGHT - 1); i++){
-    for (j = 0; j <= (WIDTH - 1); j++){
+  Pw[2] = 0; //z
+  for (y = 0; y <= (HEIGHT - 1); y++){
+    Pw[1] = ((-2*y)/(HEIGHT-1)) + 1.0; //y
+    for (x = 0; x <= (WIDTH - 1); x++){
+      Pw[0] = ((2*x)/(WIDTH-1)) - 1.0; //x
 
-      Pw[0] = ((2*j)/(WIDTH-1)) - 1.0; //x
-      Pw[1] = ((-2*i)/(HEIGHT-1)) + 1.0; //y
-      Pw[2] = 0; //z
+      De[0] = Pw[0] - Pe[0];
+      De[1] = Pw[1] - Pe[1];
+      De[2] = Pw[2] - Pe[2];
 
-      De[0] = Pw[0] - Pv[0];
-      De[1] = Pw[1] - Pv[1];
-      De[2] = Pw[2] - Pv[2];
-
-      Vtmp[0] = Pv[0] - Pc[0];
-      Vtmp[1] = Pv[1] - Pc[1];
-      Vtmp[2] = Pv[2] - Pc[2];
+      Vtmp[0] = Pe[0] - Pc[0];
+      Vtmp[1] = Pe[1] - Pc[1];
+      Vtmp[2] = Pe[2] - Pc[2];
 
       A = InnerProduct(De, De);
       B2 = 2*InnerProduct(De, Vtmp);
-      C = InnerProduct(Vtmp, Vtmp)-R*R;
+      C = InnerProduct(Vtmp, Vtmp)-(R*R);
 
-      D = B2*B2-4*A*C;
+      D = (B2*B2)-(4*A*C);
       if (D >= 0) {
         color_h = argb_to_hex(0, color);
-        my_mlx_pixel_put(&img, i, j, color_h);
+        my_mlx_pixel_put(&img, x, y, color_h);
       }
       else{ //背景
         color_h = argb_to_hex(0, RGB_back);
-        my_mlx_pixel_put(&img, i, j, color_h);
+        my_mlx_pixel_put(&img, x, y, color_h);
       }
     }
   }
